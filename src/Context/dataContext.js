68c816463bd29a800b58e8aee1b8de5e16faad78
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2023-12-20 10:26:36 -0400

    falta el logo

---
+++ src/Context/dataContext.js
@@ -0,0 +1,33 @@
+import { createContext, useContext } from "react";
+import { useLocalStorage } from "../Hooks/useLocalStorage";
+
+export const DataContext = createContext();
+
+export function DataContextProvider(props) {
+    // const [accessToken, setAccessToken] = useLocalStorage("token", '');
+    // const [accessAdminToken, setAccessAdminToken] = useLocalStorage("130328073103", '');
+    const [logged, setLogged] = useLocalStorage('log', false);
+    // const [currencyPrice, setCurrencyPrice] = useLocalStorage('currency', []);
+    // const url = 'https://remesapruebas-production.up.railway.app';
+    const value = {
+        logged, setLogged 
+        // accessToken, setAccessToken,
+        // accessAdminToken, setAccessAdminToken,
+        // currencyPrice, setCurrencyPrice,
+        // url
+    };
+
+    return (
+        <DataContext.Provider value={value}>
+            {props.children}
+        </DataContext.Provider>
+    );
+}
+
+export function useDataContext() {
+    const context = useContext(DataContext);
+    if (!context) {
+        throw new Error('useDataContext must be used within a DataContextProvider');
+    }
+    return context;
+}
