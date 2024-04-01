6cadacfeac37fac4d1fb78c7ee3425299d9af6c4
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2024-04-01 11:39:51 -0400

    linked

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -19,26 +19,26 @@ function SideBar() {
               <div role="list" className="ui selection list">
                 <div role="listitem" className="item categories__item active" id="all">
                   <div className="content">
-                    <span>Todos los productos</span>
+                    <span style={{ fontSize: '15px', color: '#212121' }}>Todos los productos</span>
                   </div>
                 </div>
                 <div role="listitem" className="item">
                   <div className="content categories__item flex ">
-                    <div>
+                    <div style={{ fontSize: '15px', color: '#212121' }}>
                       Papelería
                     </div>
                   </div>
                 </div>
                 <div role="listitem" className="item">
                   <div className="content categories__item flex ">
-                    <div>
+                    <div style={{ fontSize: '15px', color: '#212121' }}>
                       Viveres
                     </div>
                   </div>
                 </div>
                 <div role="listitem" className="item">
                   <div className="content categories__item flex ">
-                    <div>
+                    <div style={{ fontSize: '15px', color: '#212121' }}>
                       Sin categoría
                     </div>
                   </div>
@@ -52,31 +52,39 @@ function SideBar() {
               <div className="listItem">
                 <div className="link__item d-flex ">
                   <img className="icon" alt="WhatsApp" src={WhatsApp} />
-                  <div className="content" style={{cursor: 'pointer'}}>
-                    +58 412 6131828
+                  <div className="content" style={{ cursor: 'pointer' }}>
+                    <a href="https://wa.me/584126131828" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
+                      +58 412 6131828
+                    </a>
                   </div>
                 </div>
                 <div role="listitem" className="item">
                   <div className="link__item d-flex ">
                     <img className="icon" alt="Instagram" src={Instagram} />
-                    <div className="content" style={{cursor: 'pointer'}}>
-                      @lokocandy.ve
+                    <div className="content">
+                      <a href="https://www.instagram.com/lokocandy.ve" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
+                        @lokocandy.ve
+                      </a>
                     </div>
                   </div>
                 </div>
                 <div role="listitem" className="link">
                   <div className="link__item d-flex ">
                     <img className="icon" alt="Correo" src={Gmail} />
-                    <div className="content" style={{cursor: 'pointer'}}>
-                      lokocandyve@gmail.com
+                    <div className="content" style={{ cursor: 'pointer' }}>
+                      <a href="mailto:lokocandyve@gmail.com" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
+                        lokocandyve@gmail.com
+                      </a>
                     </div>
                   </div>
                 </div>
                 <div role="listitem" className="link">
                   <div className="link__item d-flex ">
                     <img className="icon" alt="Ubicación" src={Gmap} />
-                    <div className="content" style={{cursor: 'pointer'}}>
-                      Av 15 Sierra Maestra San Fco. Zulia
+                    <div className="content" style={{ cursor: 'pointer' }}>
+                      <a href="https://maps.app.goo.gl/ZkhA1FqTeE1AzBAu6" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
+                        Av 15 Sierra Maestra San Fco. Zulia
+                      </a>
                     </div>
                   </div>
                 </div>
