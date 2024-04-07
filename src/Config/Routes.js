51123955e27aa52d36ea00b8d78a4e508b5922f8
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2024-04-06 21:41:25 -0400

    Log Amd

--- src/Config/Routes.js
+++ src/Config/Routes.js
@@ -1,6 +1,12 @@
 import { Main } from '../Components/Main';
+import { LokAdm } from '../Adm/LokAdm';
 
 const Routes = [
+    {
+        title: 'LokAdm',
+        path: '/LokAdm',
+        component: LokAdm,
+    },
     {
         title: '',
         path: '/',
