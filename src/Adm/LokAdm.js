62f3257cf24e61aee53572549595d7f397b8923f
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2024-04-07 19:22:32 -0400

    fixed deploy

--- src/Adm/LokAdm.js
+++ src/Adm/LokAdm.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import LokoLogo from '../Assets/Images/LokoCandy-Logo.jpg'
 import { Link } from 'react-router-dom'
 import { Input } from 'reactstrap'
