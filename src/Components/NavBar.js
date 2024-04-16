343d2826c35cb06e5d265e402abae1147d4d680a
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2024-04-16 11:24:51 -0400

    Update Cart NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -38,51 +38,6 @@ function NavBar() {
             lokocandy
           </OffcanvasHeader>
           <OffcanvasBody>
-            {/* <div className='SideBar-Links'>
-              <h5>Contacta con nosotros</h5>
-              <section>
-                <div className="listItem">
-                  <div className="link__item d-flex ">
-                    <img className="icon" alt="WhatsApp" src={WhatsApp} />
-                    <div className="content" style={{ cursor: 'pointer' }}>
-                      <a href="https://wa.me/584126131828" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
-                        +58 412 6131828
-                      </a>
-                    </div>
-                  </div>
-                  <div role="listitem" className="item">
-                    <div className="link__item d-flex ">
-                      <img className="icon" alt="Instagram" src={Instagram} />
-                      <div className="content">
-                        <a href="https://www.instagram.com/lokocandy.ve" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
-                          @lokocandy.ve
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div role="listitem" className="link">
-                    <div className="link__item d-flex ">
-                      <img className="icon" alt="Correo" src={Gmail} />
-                      <div className="content" style={{ cursor: 'pointer' }}>
-                        <a href="mailto:lokocandyve@gmail.com" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
-                          lokocandyve@gmail.com
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div role="listitem" className="link">
-                    <div className="link__item d-flex ">
-                      <img className="icon" alt="Ubicación" src={Gmap} />
-                      <div className="content" style={{ cursor: 'pointer' }}>
-                        <a href="https://maps.app.goo.gl/ZkhA1FqTeE1AzBAu6" style={{ textDecoration: 'none', fontSize: '15px', color: '#212121' }} target="_blank" rel="noreferrer">
-                          Av 15 Sierra Maestra San Fco. Zulia
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </div> */}
             <SideBar />
           </OffcanvasBody>
         </Offcanvas>
@@ -133,9 +88,6 @@ function NavBar() {
               </div>
               <IoCartOutline size={20} />
             </i>
-            <div className='NavBar-Checkout-Cart-Price'>
-              $0.00
-            </div>
           </div>
         </div>
 
